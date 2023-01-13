7fd863fe60ec88696d5072ef81444df1ed4c5cf8
Beata Roszykiewicz <beata@magicom.com.pl>
2023-01-13 13:48:06 +0100

    Klasa niemutowalna

---
+++ kodilla-collections/src/main/java/kodilla/collections/interfaces/Circle.java
@@ -0,0 +1,24 @@
+package kodilla.collections.interfaces;
+
+public class Circle implements Shape{
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
